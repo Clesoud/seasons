9c32e62dd00c5ccb5cf79b1099ff762d94ae6162
Chris Barclay <chris.barclay@barefootsolutions.com>
2019-12-03 17:24:04 -0800

    Change App component to class (from function), add constructor function, intialize state, setState()

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (err) => console.log(err)
-  );
+class App extends React.Component {
+  // specially named function, specific to JavaScript, NOT specific to React or JSX is... constructor()
+  constructor(props) { // first function that will be called anytime an instance of this class is created.
+    super(props); // must call super in constructor function. This stops our constructor function from overriding React.Commponent's constructor function ((super is a reference to the parent's constructor function))
+    
+    this.state = { lat: null }; //setting the state object. this is the ONLY time we do direct assignment to this.state!!
 
-  return <div>Latitude: </div>;
-};
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        // we call setState to update the state!!
+        this.setState({ lat: position.coords.latitude });
+        // we did NOT do this.state.lat = pos !! only do this when initializing state in constructor function
+      },
+      err => console.log(err)
+    );
+  }
+
+  // React says we have to define render
+  render() {
+
+
+    return <div>Lattitude: {this.state.lat}</div>;
+  }
+}
 
 ReactDOM.render(
   <App />,
